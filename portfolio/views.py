5f943b2ddf136174c4419d7344f7afb5cc22865a
Ontiveros Alfaro Johán Alejandro <111895521+ChemsJam@users.noreply.github.com>
2024-01-03 16:00:43 -0600

    Estructura casi terminada

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Projects
 
 # Create your views here.
+def home(request):
+    projects = Projects.objects.all()
+    return render(request, 'home.html',{
+        'projects' : projects
+    })
+    
+
