d3dea037d07845a1feffff026a00cc1080d84edf
Ontiveros Alfaro Johan Alejandro <johalfaro1174@gmail.com>
2024-10-22 12:13:58 -0600

    SQLite Conection.

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'django_portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_portfolio',
-        'USER': 'postgres',
-        'PASSWORD': 'Soyjohan123.',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
 
