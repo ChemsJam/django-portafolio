1a8c4908d9b85d6702bbb83c3944cbadff3ce196
Ontiveros Alfaro Johan Alejandro <johalfaro1174@gmail.com>
2024-10-22 11:25:42 -0600

    My personal info updated.

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_portfolio',
+        'USER': 'postgres',
+        'PASSWORD': 'Soyjohan123.',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
