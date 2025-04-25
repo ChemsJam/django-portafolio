c7a1470198e9a86203470001f7b7c44b6b4452fb
Ontiveros Alfaro Johan Alejandro <johalfaro1174@gmail.com>
2025-04-25 13:06:53 -0600

    Dates adeed to work expirence

--- workexpirence/models.py
+++ workexpirence/models.py
@@ -12,3 +12,5 @@ class WorkExpirence(models.Model):
     technologies = models.ManyToManyField(Technology, related_name="all_technologies_workexpirence")
     image = ImageField(upload_to='portfolio/images')
     location = URLField(blank=True)
+    initial_date = models.DateField(blank=True, null=True)
+    final_date = models.DateField(blank=True, null=True)
